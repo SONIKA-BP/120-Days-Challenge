555f67982410e2df5a4428f24fb9445ab2ccfeb0
Sonika B P <158311073+SONIKA-BP@users.noreply.github.com>
2025-04-09 17:53:14 +0530

    Add files via upload

---
+++ Day42.java
@@ -0,0 +1,10 @@
+
+public class Day42{
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+
+    }
+    public static void main(String[] args){
+        System.out.println(strStr("Sonika B P", "Son"));
+    }
+}
